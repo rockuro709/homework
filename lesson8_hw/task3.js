d8d33d1ce9f12c7bf84796a06896304fa5fb95f1
Anton Tratsevskii <atratsevskii@gmail.com>
2025-03-24 19:06:19 +0300

    lesson8_hw update: get rid of code duplication

--- lesson8_hw/task3.js
+++ lesson8_hw/task3.js
@@ -1,4 +1,4 @@
-function getNum15 () {
+/* function getNum15 () {
     return new Promise((resolve) => {
         setTimeout(() => {
             resolve(Math.floor(Math.random() * (6 - 1) + 1));
@@ -12,11 +12,26 @@ function getNum610 () {
             resolve(Math.floor(Math.random() * (11 - 6) + 6));
         }, 5000);
     });
+} */
+
+function getNum (firstNum, lastNum, delay) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(Math.floor(Math.random() * (lastNum - firstNum + 1) + firstNum));
+        }, delay);
+    });
 }
 
-async function getResult() {
+/* async function getResult() {
     const result1 = await getNum15();
     const result2 = await getNum610();
+    console.log(result1 + result2);
+} */
+
+async function getResult() {
+    const result1 = await getNum(1, 5, 3000);
+    const result2 = await getNum(6, 10, 5000);
+    console.log(result1, result2);
     console.log(result1 + result2);
 }
 
