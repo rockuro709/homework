2073e0b586c6c9f79f85aa9cb389dbd0dbfda3a3
Anton Tratsevskii <atratsevskii@gmail.com>
2025-03-30 18:14:32 +0300

    leeson12_hw_done

---
+++ lesson12_hw/tests/alerts.spec.js
@@ -0,0 +1,32 @@
+import { test, expect } from '@playwright/test';
+import { testData } from './testdata.js';
+
+
+test.describe('demoqa.com/alerts tests', async () => {
+
+    test.beforeEach(async ({page}) => {
+        //добавил waitUntil, потому что индикатор загрузки почему-то крутится очень долго и приходится ждать 20 секунд лишних
+        await page.goto('https://demoqa.com/alerts', {waitUntil: 'domcontentloaded'});
+    });
+
+    test('should choose Alerts, click on confirm-box-button, show dialog/alert with certain message, confirm alert and show selected choice', async ({page}) => {
+        await page.locator('.btn.btn-light.active').click();
+        page.on('dialog', async (dialog) => {
+            expect(dialog.message()).toBe('Do you confirm action?'); 
+            await dialog.accept();
+        });
+        await page.locator('#confirmButton').click();
+        await expect(page.locator('#confirmResult')).toHaveText(/.+Ok/);
+    });
+
+    test('should choose Alerts, click on prompt-box-button, show dialog/alert with certain message, input data, confirm alert and show entered data', async ({page}) => {
+        await page.locator('.btn.btn-light.active').click();
+        page.on('dialog', async (dialog) => {
+            expect(dialog.message()).toBe('Please enter your name'); 
+            await dialog.accept(testData.promptData);
+        });
+        await page.locator('#promtButton').click();
+        await expect(page.locator('#promptResult')).toHaveText(new RegExp(`.+${testData.promptData}`));
+    });
+
+});
