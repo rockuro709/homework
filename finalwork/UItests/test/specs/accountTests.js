59039c398005c4adba99395ca05e0bba49b3438b
Anton Tratsevskii <atratsevskii@gmail.com>
2025-05-04 17:13:57 +0300

    wdio expect

--- finalwork/UItests/test/specs/accountTests.js
+++ finalwork/UItests/test/specs/accountTests.js
@@ -1,9 +1,8 @@
+import { expect } from "@wdio/globals";
 import Header from "../pageobjects/components/header.js";
 import TestData from "../testData/testData.js";
 import SignupPage from "../pageobjects/signupPage.js";
-import { expect } from "chai";
 import SignupLoginPage from "../pageobjects/signupLoginPage.js";
-import signupLoginPage from "../pageobjects/signupLoginPage.js";
 
 describe("User Account Tests", () => {
   before(async () => {
@@ -17,40 +16,38 @@ describe("User Account Tests", () => {
 
   it("logs out from the account and verifies logout was successful", async () => {
     await Header.logout();
-    expect(await Header.signupOrLoginButton.isDisplayed()).to.be.true;
+    await expect(Header.signupOrLoginButton).toBeDisplayed();
   });
 
   it("displays an error when registering with an already registered email", async () => {
     await SignupLoginPage.newUserSignup(TestData.fastUser);
-    expect(await SignupLoginPage.signupErrorMessage.isDisplayed()).to.be.true;
+    await expect(SignupLoginPage.signupErrorMessage).toBeDisplayed();
   });
 
-  it("displays an error when loging with an incorrect email or password", async () => {
-    await signupLoginPage.login(TestData.fullUser);
-    expect(await SignupLoginPage.loginErrorMessage.isDisplayed()).to.be.true;
-  })
+  it("displays an error when logging in with an incorrect email or password", async () => {
+    await SignupLoginPage.login(TestData.fullUser);
+    await expect(SignupLoginPage.loginErrorMessage).toBeDisplayed();
+  });
 
   it("registers a new user and verifies successful login and account deletion", async () => {
     await SignupPage.createNewAccount(TestData.fullUser);
-    expect(await Header.loggedInAs.getText()).to.equal(
-      `${TestData.fullUser.name}`
-    );
+    await expect(Header.loggedInAs).toHaveText(`${TestData.fullUser.name}`);
 
     await Header.deleteAccount();
-    expect(await Header.signupOrLoginButton.isDisplayed()).to.be.true;
+    await expect(Header.signupOrLoginButton).toBeDisplayed();
+
     await SignupLoginPage.login(TestData.fullUser);
-    expect(await SignupLoginPage.loginErrorMessage.isDisplayed()).to.be.true;
+    await expect(SignupLoginPage.loginErrorMessage).toBeDisplayed();
   });
 
   it("logs in with an existing user, verifies session, and deletes the account", async () => {
     await SignupLoginPage.login(TestData.fastUser);
-    expect(await Header.loggedInAs.getText()).to.equal(
-      `${TestData.fastUser.name}`
-    );
+    await expect(Header.loggedInAs).toHaveText(`${TestData.fastUser.name}`);
 
     await Header.deleteAccount();
-    expect(await Header.signupOrLoginButton.isDisplayed()).to.be.true;
+    await expect(Header.signupOrLoginButton).toBeDisplayed();
+
     await SignupLoginPage.login(TestData.fastUser);
-    expect(await SignupLoginPage.loginErrorMessage.isDisplayed()).to.be.true;
+    await expect(SignupLoginPage.loginErrorMessage).toBeDisplayed();
   });
 });
